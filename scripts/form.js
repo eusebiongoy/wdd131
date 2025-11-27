0b77feaa73e6975ec4de14e6b8316786d0ce8378
Eusebio Ngoy <banzaeusebio2@gmail.com>
2025-11-28 00:14:16 +0200

    change

--- scripts/form.js
+++ scripts/form.js
@@ -1,1 +1,11 @@
 document.getElementById("lastModified").innerHTML = document.lastModified;
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
