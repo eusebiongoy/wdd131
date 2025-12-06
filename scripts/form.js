2af6f879582bfed48fdd97c11a24872159f0e77d
Eusebio Ngoy <banzaeusebio2@gmail.com>
2025-12-06 09:02:06 +0200

    selection

--- scripts/form.js
+++ scripts/form.js
@@ -1,8 +1,28 @@
 document.getElementById("lastModified").innerHTML = document.lastModified;
-const rangevalue = document.getElementById("rangevalue");
-const range = document.getElementById("r");
-
-
-function displayRatingValue() {
-    rangevalue.innerHTML = range.value;
-}
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
